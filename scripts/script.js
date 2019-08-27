e8991e55357145eb59337b667f6a63ab542885e6
Hsuan-Hau Liu <hsuanhal@usc.edu>
2019-08-27 12:25:39 -0700

    update projects section

--- scripts/script.js
+++ scripts/script.js
@@ -7,4 +7,16 @@ var switch_tab = function(tab) {
       t.style.display = "none";
     }
   });
+
+  var btn_id = 'btn-' + tab;
+  var active = "btn btn-primary tab-btn";
+  var inactive = "btn btn-secondary tab-btn";
+  var tab_btns = document.getElementsByClassName("tab-btn");
+  Array.prototype.forEach.call(tab_btns, function(tb) {
+    if (tb.id == btn_id) {
+      tb.className = active;
+    } else {
+      tb.className = inactive;
+    }
+  });
 }

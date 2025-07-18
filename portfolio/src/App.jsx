04476637c853026c3e8ccd7c8c0d45facf14478e
Howard Liu <23202168+hsuanhauliu@users.noreply.github.com>
2025-07-17 22:45:03 -0700

    chore: re-organize code structure to support multi tabs

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,11 +1,40 @@
-import PortfolioPage from './components/PortfolioPage'; 
+import { useRef } from 'react';
+import StarrySkyContainer from './components/background/StarrySkyContainer';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import PortfolioPage from './components/PortfolioPage';
 
-function App() {
+import './styles/background/StarrySkyContainer.css'
+import './App.css'
+
+export default function App() {
+  const sectionLinks = [
+    { "id": "#about", "name": "About" },
+    { "id": "#experience", "name": "Experience" },
+    { "id": "#skills", "name": "Skills" },
+    { "id": "#projects", "name": "Projects" },
+    { "id": "#contact", "name": "Contact" },
+  ];
+
+  // An object to store refs for each section to calculate scroll positions
+  const sectionRefs = {
+    about: useRef(null),
+    experience: useRef(null),
+    skills: useRef(null),
+    projects: useRef(null),
+    contact: useRef(null),
+  };
+
   return (
-    <div className="App">
-      <PortfolioPage />
+    <div className="antialiased">
+      <StarrySkyContainer />
+
+      <Header sectionLinks={sectionLinks} sectionRefs={sectionRefs} />
+
+      <PortfolioPage sectionRefs={sectionRefs} />
+
+      <Footer />
     </div>
   );
 }
 
-export default App;
